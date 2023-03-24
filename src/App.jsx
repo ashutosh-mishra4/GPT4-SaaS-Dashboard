716e2e2e96064afc84e38643237a5e24f5459490
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2023-03-24 16:38:35 +0530

    Add Menu Sidebar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MenuSidebar from "./components/MenuSidebar";
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex">
+      <MenuSidebar />
+      {/* Your main content component */}
+    </div>
+  );
+}
+
+export default App;
